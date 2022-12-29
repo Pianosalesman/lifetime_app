b61800cf6098e837194eb1727c305664f629d949
Pianosalesman <andreydavydov1@gmail.com>
2022-12-29 18:07:06 +0300

    Проблемы с написанием кода на Бэкенд

--- database.py
+++
@@ -1,8 +0,0 @@
-import psycopg2
-
-
-def get_db_connection():
-    connection = psycopg2.connect(
-        user="postgres", password="postgres", host="127.0.0.1", port="5432", database="postgres"
-    )
-    return connection
